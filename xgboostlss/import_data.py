59460d07790e25aea2bf48710fb7ff841b242367
Alexander März <alex.maerz@gmx.net>
2021-11-14 16:24:01 +0100

    Create import_data.py

---
+++ xgboostlss/import_data.py
@@ -0,0 +1,20 @@
+import pkg_resources
+import pandas as pd
+
+
+def load_simulated_data():
+    """Returns train/test dataframe of a simulated example.
+
+    Contains the following columns:
+        y              int64: response
+        x              int64: x-feature
+        X1:X10         int64: random noise features
+
+    """
+    train_path = pkg_resources.resource_stream(__name__, 'datasets/train_sim.csv')
+    train_df = pd.read_csv(train_path)
+
+    test_path = pkg_resources.resource_stream(__name__, 'datasets/test_sim.csv')
+    test_df = pd.read_csv(test_path)
+
+    return train_df, test_df
