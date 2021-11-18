9b8991877a5f425f59165771a1bae21e29d83def
Alexander März <alex.maerz@gmx.net>
2021-11-18 19:11:46 +0100

    Updated data loader

--- xgboostlss/datasets/data_loader.py
+++ xgboostlss/datasets/data_loader.py
@@ -11,10 +11,10 @@ def load_simulated_data():
         X1:X10         int64: random noise features
 
     """
-    train_path = pkg_resources.resource_stream(__name__, 'datasets/train_sim.csv')
+    train_path = pkg_resources.resource_stream(__name__, 'train_sim.csv')
     train_df = pd.read_csv(train_path)
 
-    test_path = pkg_resources.resource_stream(__name__, 'datasets/test_sim.csv')
+    test_path = pkg_resources.resource_stream(__name__, 'test_sim.csv')
     test_df = pd.read_csv(test_path)
 
     return train_df, test_df
