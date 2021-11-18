6fc5318b98213d08a6955f9a333d3b0f7fc508f8
Alexander März <alex.maerz@gmx.net>
2021-11-18 19:50:20 +0100

    Update utils.py

--- xgboostlss/utils.py
+++ xgboostlss/utils.py
@@ -14,15 +14,15 @@ def soft_plus(predt: np.ndarray):
     '''Softplus function used to ensure predt is strictly positive.
 
     '''
-    safe_softplus = np.log1p(np.exp(-np.abs(x))) + np.maximum(x, 0)
-    safe_softplus[safe_softplus == 0] = 1e-15
-    return safe_softplus
+    predt = np.log1p(np.exp(-np.abs(predt))) + np.maximum(predt, 0)
+    predt[predt == 0] = 1e-15
+    return predt
 
 
 def soft_plus1(predt: np.ndarray):
     '''Softplus function used to ensure predt is greater than 1.
 
     '''
-    safe_softplus = np.log1p(np.exp(-np.abs(x))) + np.maximum(x, 0)
-    safe_softplus[safe_softplus <= 1] = 1 + 1e-15
-    return safe_softplus
+    predt = np.log1p(np.exp(-np.abs(predt))) + np.maximum(predt, 0)
+    predt[predt <= 1] = 1 + 1e-15
+    return predt
