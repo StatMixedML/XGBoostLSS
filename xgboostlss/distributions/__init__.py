0d2ee30cc0800a986e63e31e87083e44a0b49316
Alexander März <alex.maerz@gmx.net>
2021-12-19 10:46:38 +0100

    Update __init__.py

--- xgboostlss/distributions/__init__.py
+++ xgboostlss/distributions/__init__.py
@@ -1,7 +1,8 @@
 """XGBoostLSS - An extension of XGBoost to probabilistic forecasting"""
 
 from xgboostlss.distributions.Gaussian import *
+from xgboostlss.distributions.Gaussian_AutoGrad import *
 from xgboostlss.distributions.StudentT import *
 from xgboostlss.distributions.BCT import *
 from xgboostlss.distributions.Beta import *
-from xgboostlss.distributions.NegativeBinomial import *
+from xgboostlss.distributions.NegativeBinomial import * 
