ed5fb1857f51f633511f2a5ad3d13f5658356a6b
Alexander März <alex.maerz@gmx.net>
2021-12-19 11:19:48 +0100

    Update __init__.py

--- xgboostlss/distributions/__init__.py
+++ xgboostlss/distributions/__init__.py
@@ -5,4 +5,4 @@ from xgboostlss.distributions.Gaussian_AutoGrad import *
 from xgboostlss.distributions.StudentT import *
 from xgboostlss.distributions.BCT import *
 from xgboostlss.distributions.Beta import *
-from xgboostlss.distributions.NegativeBinomial import * 
+from xgboostlss.distributions.NegativeBinomial import *
