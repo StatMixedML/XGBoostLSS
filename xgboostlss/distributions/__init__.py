82b6be0bfddb66b78134b86839d7d735f387e78b
Alexander März <alex.maerz@gmx.net>
2021-12-22 14:49:49 +0100

    Update __init__.py
    Added Expectile Distribution

--- xgboostlss/distributions/__init__.py
+++ xgboostlss/distributions/__init__.py
@@ -6,3 +6,4 @@ from xgboostlss.distributions.StudentT import *
 from xgboostlss.distributions.BCT import *
 from xgboostlss.distributions.Beta import *
 from xgboostlss.distributions.NegativeBinomial import *
+from xgboostlss.distributions.Expectile import *
