ed2f53b9592201cb59124dfae8f7d6083af74a2f
Cattes <sebastian.cattes@inwt-statistics.de>
2022-07-19 17:47:04 +0200

    add scikit-learn for shap dependency

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(
         "numpy>=1.21.6",
         "pandas>=1.3.5",
         "scipy>=1.7.3",
+        "scikit-learn>=1.1.1",
     ],
     test_suite="tests",
     tests_require=["flake8", "pytest"],
