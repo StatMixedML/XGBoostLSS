06d9b91c2229e7224b98f3c82fd21e4c9f21d703
Alexander März <alex.maerz@gmx.net>
2023-05-18 08:38:07 +0200

    Update to release 0.2.0

--- xgboostlss/datasets/data_loader.py
+++ xgboostlss/datasets/data_loader.py
@@ -2,8 +2,9 @@ import pkg_resources
 import pandas as pd
 
 
-def load_simulated_data():
-    """Returns train/test dataframe of a simulated example.
+def load_simulated_gaussian_data():
+    """
+    Returns train/test dataframe of a simulated example.
 
     Contains the following columns:
         y              int64: response
@@ -11,10 +12,29 @@ def load_simulated_data():
         X1:X10         int64: random noise features
 
     """
-    train_path = pkg_resources.resource_stream(__name__, 'train_sim.csv')
+    train_path = pkg_resources.resource_stream(__name__, "gaussian_train_sim.csv")
     train_df = pd.read_csv(train_path)
 
-    test_path = pkg_resources.resource_stream(__name__, 'test_sim.csv')
+    test_path = pkg_resources.resource_stream(__name__, "gaussian_test_sim.csv")
+    test_df = pd.read_csv(test_path)
+
+    return train_df, test_df
+
+
+def load_simulated_studentT_data():
+    """
+    Returns train/test dataframe of a simulated example.
+
+    Contains the following columns:
+        y              int64: response
+        x              int64: x-feature
+        X1:X10         int64: random noise features
+
+    """
+    train_path = pkg_resources.resource_stream(__name__, "studentT_train_sim.csv")
+    train_df = pd.read_csv(train_path)
+
+    test_path = pkg_resources.resource_stream(__name__, "studentT_test_sim.csv")
     test_df = pd.read_csv(test_path)
 
     return train_df, test_df
