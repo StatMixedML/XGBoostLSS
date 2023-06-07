794ee8e88fd18fe8b7a6dff52dfc7956a262f59e
Alexander März <alex.maerz@gmx.net>
2023-06-07 10:50:37 +0200

    Added Zero Inflated Poisson distribution

---
+++ xgboostlss/distributions/ZIP.py
@@ -0,0 +1,59 @@
+from .zero_inflated import ZeroInflatedPoisson as ZeroInflatedPoisson_Torch
+from xgboostlss.utils import *
+from .distribution_utils import *
+
+class ZIP:
+    """
+    Zero-Inflated Poisson distribution class.
+
+    Distributional Parameters
+    -------------------------
+    rate: torch.Tensor
+        Rate parameter of the distribution (often referred to as lambda).
+    gate: torch.Tensor
+        Probability of extra zeros given via a Bernoulli distribution.
+
+    Source
+    -------------------------
+    - https://github.com/pyro-ppl/pyro/blob/dev/pyro/distributions/zero_inflated.py#L121
+
+    Parameters
+    -------------------------
+    stabilization: str
+        Stabilization method for the Gradient and Hessian. Options are "None", "MAD", "L2".
+    response_fn: str
+        When a custom objective and metric are provided, XGBoost doesn't know its response and link function. Hence,
+        the user is responsible for specifying the transformations. Options are "exp", "softplus" or "relu".
+    """
+    def __init__(self,
+                 stabilization: str = "None",
+                 response_fn: str = "relu"
+                 ):
+        # Check Response Function
+        if response_fn == "exp":
+            response_fn = exp_fn
+            inverse_response_fn = log_fn
+        elif response_fn == "softplus":
+            response_fn = softplus_fn
+            inverse_response_fn = softplusinv_fn
+        elif response_fn == "relu":
+            response_fn = relu_fn
+            inverse_response_fn = reluinv_fn
+        else:
+            raise ValueError("Invalid response function for total_count. Please choose from 'exp', 'softplus' or relu.")
+
+        # Specify Response and Link Functions
+        param_dict = {"rate": response_fn, "gate": sigmoid_fn}
+        param_dict_inv = {"rate": inverse_response_fn, "gate": sigmoidinv_fn}
+        distribution_arg_names = list(param_dict.keys())
+
+        # Specify Distribution
+        self.dist_class = DistributionClass(distribution=ZeroInflatedPoisson_Torch,
+                                            univariate=True,
+                                            discrete=True,
+                                            n_dist_param=len(param_dict),
+                                            stabilization=stabilization,
+                                            param_dict=param_dict,
+                                            param_dict_inv=param_dict_inv,
+                                            distribution_arg_names=distribution_arg_names
+                                            )
