c00b7d0d160ede87f69d06c8fbc1be208b02af35
Alexander März <alex.maerz@gmx.net>
2023-06-14 10:17:00 +0200

    Renaming

--- xgboostlss/distributions/ZAGamma.py
+++ xgboostlss/distributions/ZAGamma.py
@@ -1,21 +1,26 @@
-from .zero_inflated import ZeroInflatedPoisson as ZeroInflatedPoisson_Torch
+from .zero_inflated import ZeroAdjustedGamma as ZeroAdjustedGamma_Torch
 from xgboostlss.utils import *
 from .distribution_utils import *
 
-class ZIP:
-    """
-    Zero-Inflated Poisson distribution class.
 
-    Distributional Parameters
-    -------------------------
+class ZAGamma:
+    """
+    Zero-Adjusted Gamma distribution class.
+
+    The zero-adjusted Gamma distribution is similar to the Gamma distribution but allows zeros as y values.
+
+     Distributional Parameters
+    --------------------------
+    concentration: torch.Tensor
+        shape parameter of the distribution (often referred to as alpha)
     rate: torch.Tensor
-        Rate parameter of the distribution (often referred to as lambda).
+        rate = 1 / scale of the distribution (often referred to as beta)
     gate: torch.Tensor
-        Probability of extra zeros given via a Bernoulli distribution.
+        Probability of zeros given via a Bernoulli distribution.
 
     Source
     -------------------------
-    https://github.com/pyro-ppl/pyro/blob/dev/pyro/distributions/zero_inflated.py#L121
+    https://github.com/pyro-ppl/pyro/blob/dev/pyro/distributions/zero_inflated.py#L150
 
     Parameters
     -------------------------
@@ -23,7 +28,7 @@ class ZIP:
         Stabilization method for the Gradient and Hessian. Options are "None", "MAD", "L2".
     response_fn: str
         When a custom objective and metric are provided, XGBoost doesn't know its response and link function. Hence,
-        the user is responsible for specifying the transformations. Options are "exp", "softplus" or "relu".
+        the user is responsible for specifying the transformations. Options are "exp" or "softplus".
     loss_fn: str
         Loss function. Options are "nll" (negative log-likelihood) or "crps" (continuous ranked probability score).
         Note that if "crps" is used, the Hessian is set to 1, as the current CRPS version is not twice differentiable.
@@ -31,7 +36,7 @@ class ZIP:
     """
     def __init__(self,
                  stabilization: str = "None",
-                 response_fn: str = "relu",
+                 response_fn: str = "exp",
                  loss_fn: str = "nll"
                  ):
         # Check Response Function
@@ -41,21 +46,18 @@ class ZIP:
         elif response_fn == "softplus":
             response_fn = softplus_fn
             inverse_response_fn = softplusinv_fn
-        elif response_fn == "relu":
-            response_fn = relu_fn
-            inverse_response_fn = reluinv_fn
         else:
-            raise ValueError("Invalid response function for total_count. Please choose from 'exp', 'softplus' or relu.")
+            raise ValueError("Invalid response function. Please choose from 'exp' or 'softplus'.")
 
         # Specify Response and Link Functions
-        param_dict = {"rate": response_fn, "gate": sigmoid_fn}
-        param_dict_inv = {"rate": inverse_response_fn, "gate": sigmoidinv_fn}
+        param_dict = {"concentration": response_fn, "rate": response_fn, "gate": sigmoid_fn}
+        param_dict_inv = {"concentration": inverse_response_fn, "rate": inverse_response_fn, "gate": sigmoidinv_fn}
         distribution_arg_names = list(param_dict.keys())
 
         # Specify Distribution
-        self.dist_class = DistributionClass(distribution=ZeroInflatedPoisson_Torch,
+        self.dist_class = DistributionClass(distribution=ZeroAdjustedGamma_Torch,
                                             univariate=True,
-                                            discrete=True,
+                                            discrete=False,
                                             n_dist_param=len(param_dict),
                                             stabilization=stabilization,
                                             param_dict=param_dict,
--- xgboostlss/distributions/ZIPoisson.py
+++ xgboostlss/distributions/ZIPoisson.py
@@ -1,26 +1,21 @@
-from .zero_inflated import ZeroAdjustedGamma as ZeroAdjustedGamma_Torch
+from .zero_inflated import ZeroInflatedPoisson as ZeroInflatedPoisson_Torch
 from xgboostlss.utils import *
 from .distribution_utils import *
 
-
-class ZAGA:
+class ZIPoisson:
     """
-    Zero-Adjusted Gamma distribution class.
+    Zero-Inflated Poisson distribution class.
 
-    The zero-adjusted Gamma distribution is similar to the Gamma distribution but allows zeros as y values.
-
-     Distributional Parameters
-    --------------------------
-    concentration: torch.Tensor
-        shape parameter of the distribution (often referred to as alpha)
+    Distributional Parameters
+    -------------------------
     rate: torch.Tensor
-        rate = 1 / scale of the distribution (often referred to as beta)
+        Rate parameter of the distribution (often referred to as lambda).
     gate: torch.Tensor
-        Probability of zeros given via a Bernoulli distribution.
+        Probability of extra zeros given via a Bernoulli distribution.
 
     Source
     -------------------------
-    https://github.com/pyro-ppl/pyro/blob/dev/pyro/distributions/zero_inflated.py#L150
+    https://github.com/pyro-ppl/pyro/blob/dev/pyro/distributions/zero_inflated.py#L121
 
     Parameters
     -------------------------
@@ -28,7 +23,7 @@ class ZAGA:
         Stabilization method for the Gradient and Hessian. Options are "None", "MAD", "L2".
     response_fn: str
         When a custom objective and metric are provided, XGBoost doesn't know its response and link function. Hence,
-        the user is responsible for specifying the transformations. Options are "exp" or "softplus".
+        the user is responsible for specifying the transformations. Options are "exp", "softplus" or "relu".
     loss_fn: str
         Loss function. Options are "nll" (negative log-likelihood) or "crps" (continuous ranked probability score).
         Note that if "crps" is used, the Hessian is set to 1, as the current CRPS version is not twice differentiable.
@@ -36,7 +31,7 @@ class ZAGA:
     """
     def __init__(self,
                  stabilization: str = "None",
-                 response_fn: str = "exp",
+                 response_fn: str = "relu",
                  loss_fn: str = "nll"
                  ):
         # Check Response Function
@@ -46,18 +41,21 @@ class ZAGA:
         elif response_fn == "softplus":
             response_fn = softplus_fn
             inverse_response_fn = softplusinv_fn
+        elif response_fn == "relu":
+            response_fn = relu_fn
+            inverse_response_fn = reluinv_fn
         else:
-            raise ValueError("Invalid response function. Please choose from 'exp' or 'softplus'.")
+            raise ValueError("Invalid response function for total_count. Please choose from 'exp', 'softplus' or relu.")
 
         # Specify Response and Link Functions
-        param_dict = {"concentration": response_fn, "rate": response_fn, "gate": sigmoid_fn}
-        param_dict_inv = {"concentration": inverse_response_fn, "rate": inverse_response_fn, "gate": sigmoidinv_fn}
+        param_dict = {"rate": response_fn, "gate": sigmoid_fn}
+        param_dict_inv = {"rate": inverse_response_fn, "gate": sigmoidinv_fn}
         distribution_arg_names = list(param_dict.keys())
 
         # Specify Distribution
-        self.dist_class = DistributionClass(distribution=ZeroAdjustedGamma_Torch,
+        self.dist_class = DistributionClass(distribution=ZeroInflatedPoisson_Torch,
                                             univariate=True,
-                                            discrete=False,
+                                            discrete=True,
                                             n_dist_param=len(param_dict),
                                             stabilization=stabilization,
                                             param_dict=param_dict,
