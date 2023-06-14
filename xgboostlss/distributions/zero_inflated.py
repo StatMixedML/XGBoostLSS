b192ad019a0bce5a3087bfc1d3d27a09b4ae10fc
Alexander März <alex.maerz@gmx.net>
2023-06-14 17:46:15 +0200

    Updated boundary_checks

--- xgboostlss/distributions/zero_inflated.py
+++ xgboostlss/distributions/zero_inflated.py
@@ -93,10 +93,10 @@ class ZeroInflatedDistribution(TorchDistribution):
 
         if "gate" in self.__dict__:
             gate, value = broadcast_all(self.gate, value)
-            log_prob = torch.log1p(1 - gate) + self.base_dist.log_prob(value)
-            log_prob = torch.where(zero_idx, torch.log1p(gate), log_prob)
-            # log_prob = (-gate).log1p() + self.base_dist.log_prob(value)
-            # log_prob = torch.where(zero_idx == True, (gate + log_prob.exp()).log(), log_prob)
+            # log_prob = torch.log1p(1 - gate) + self.base_dist.log_prob(value)
+            # log_prob = torch.where(zero_idx, torch.log1p(gate), log_prob)
+            log_prob = (-gate).log1p() + self.base_dist.log_prob(value)
+            log_prob = torch.where(zero_idx == True, (gate + log_prob.exp()).log(), log_prob)
         else:
             gate_logits, value = broadcast_all(self.gate_logits, value)
             log_prob_minus_log_gate = -gate_logits + self.base_dist.log_prob(value)
