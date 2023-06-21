b24a1a6398aadc3d37553329d1ba20bc8b5d4bd3
Alexander März <alex.maerz@gmx.net>
2023-06-21 15:45:16 +0200

    Added multivariate datasets

--- xgboostlss/datasets/data_loader.py
+++ xgboostlss/datasets/data_loader.py
@@ -38,3 +38,58 @@ def load_simulated_studentT_data():
     test_df = pd.read_csv(test_path)
 
     return train_df, test_df
+
+
+def load_simulated_multivariate_gaussian_data():
+    """
+    Returns train/test dataframe of a simulated example.
+
+    Contains the following columns:
+        y              int64: response
+        x              int64: x-feature
+
+    """
+    data_path = pkg_resources.resource_stream(__name__, "sim_triv_gaussian.csv")
+    data_df = pd.read_csv(data_path)
+
+    return data_df
+
+
+def load_simulated_multivariate_studentT_data():
+    """
+    Returns train/test dataframe of a simulated example.
+
+    Contains the following columns:
+        y              int64: response
+        x              int64: x-feature
+
+    """
+    data_path = pkg_resources.resource_stream(__name__, "sim_triv_studentT.csv")
+    data_df = pd.read_csv(data_path)
+
+    return data_df
+
+
+def load_articlake_data():
+    """
+    Returns the arctic lake sediment data: sand, silt, clay compositions of 39 sediment samples at different water
+    depths in an Arctic lake.
+
+    Contains the following columns:
+        sand: numeric
+            Vector of percentages of sand.
+        silt: numeric
+            Vector of percentages of silt.
+        clay: numeric
+            Vector of percentages of clay
+        depth: numeric
+            Vector of water depths (meters) in which samples are taken.
+
+    Source
+    ------
+    https://rdrr.io/rforge/DirichletReg/
+    """
+    data_path = pkg_resources.resource_stream(__name__, "arcticlake.csv")
+    data_df = pd.read_csv(data_path)
+
+    return data_df
